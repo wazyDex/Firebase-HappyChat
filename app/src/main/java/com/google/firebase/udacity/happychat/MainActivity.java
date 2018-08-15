a048ae0fa2fd1f751e6b2a14cc7eca555c802314
Dex.Wasy <dexwasy@gmail.com>
2018-08-15 12:32:53 -0400

    push

--- app/src/main/java/com/google/firebase/udacity/happychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/happychat/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
-//
+
     public static final int RC_SIGN_IN = 1;
 
     private ListView mMessageListView;
